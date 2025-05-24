b3d53a33592a91d87a08cb464f09d40019f9b0d1
Deepak Saini <deepak170602@gmail.com>
2025-05-24 15:06:01 +0530

    strict=True enabled

--- src/voice_agent/llm/tools/tools_schema.py
+++ src/voice_agent/llm/tools/tools_schema.py
@@ -6,9 +6,15 @@ schema = {
             "description": "Thanks callee for their time and ends or hangs up the phone call.",
             "parameters": {
                 "type": "object",
-                "properties": {},
+                "properties": {
+                    "msg": {
+                        "type": "string",
+                        "description": "A polite message spoken to the callee before hanging up the call. This message should express gratitude and provide a courteous closing."
+                    }    
+                },
                 "additionalProperties": False
-            }
+            },
+            "strict": True
         }
     }
 }   
